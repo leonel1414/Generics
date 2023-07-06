dab5750296d0ae3af6742f494d98c570c86562c4
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-06 20:25:04 -0300

    Agregar archivos de proyecto.

---
+++ Generics/Program.cs
@@ -0,0 +1,10 @@
+﻿//Generics
+Generic<string> str = new Generic<string>();
+str.Dato = "Dato";
+Console.WriteLine($"Generics Dato: {str.Dato}");
+Console.WriteLine($"Tipo de Generic Dato: {str.Dato.GetType().FullName}");
+
+public class Generic<T> { 
+    public T Dato { get; set; }
+}
+
