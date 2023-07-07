80491824512fb6f155b083f4357db54620cdee2a
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-06 21:11:18 -0300

    Modificaciones sobre herencia

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,4 +1,5 @@
-﻿//Generics
+﻿/*
+ //Generics
 Generic<string> str = new Generic<string>();
 str.Dato = "Dato";
 Console.WriteLine($"Generics Dato: {str.Dato}");
@@ -8,3 +9,40 @@ public class Generic<T> {
     public T Dato { get; set; }
 }
 
+*/
+
+/*
+ * HERENCIA
+ */
+Vehiculo vehiculo = new Vehiculo();
+vehiculo.Arrancar();
+
+
+Coche coche = new Coche();
+coche.Arrancar();
+coche.DetenerCoche();
+
+
+class Vehiculo { 
+public float combustible { get; set; }
+ public int aforo { get; set; }
+
+
+    public void Arrancar() {
+        Console.WriteLine("Este vehiculo ha arrancado");
+    }
+}
+
+
+class Coche : Vehiculo { 
+    public int ruedas { get; set; }
+
+    public void DetenerCoche() { 
+        Console.WriteLine("Se ha detenido el vehiculo");
+    }
+
+    public void Arrancar() { 
+    Console.WriteLine("Esté coche ha arrancado con éxito!!");
+    }
+
+}
